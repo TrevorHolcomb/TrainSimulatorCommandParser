da6993817b69b7672c78a55057be881107e3b18d
Trevor Holcomb <seaggo@gmail.com>
2016-02-28 10:15:08 -0800

    Added files via upload

---
+++ CommandParser.java
@@ -0,0 +1,478 @@
+/* Team 1
+   CommandParser class
+*/
+
+package w16cs350.controller.cli.parser;
+import w16cs350.controller.command.*;
+import w16cs350.controller.command.creational.*;
+import w16cs350.controller.command.meta.*;
+import w16cs350.controller.command.behavioral.*;
+import w16cs350.controller.command.structural.*;
+import w16cs350.datatype.Angle;
+import w16cs350.controller.cli.*;
+
+import java.util.*;
+
+public class CommandParser
+{
+   //private A_ParserHelper parserHelper;
+   private String commandText;
+
+   public CommandParser(String commandText)
+   {
+      // I took out the parserHelper for the time being so
+      // that we can actually test our strings by only passing through a string
+      //this.parserHelper = parserHelper;
+      this.commandText = commandText;
+   }
+   
+   public void parse()
+   {
+      String[] commandArray = this.commandText.split("\\s+");
+      String subCommand;
+      switch (commandArray[0].toUpperCase()){
+         case "DO":
+            subCommand = createSubCommand(commandArray);
+            // this is where you would do this.parserHelper.getActionProcessor().schedule(createBehavioralCommand(subCommand));
+            A_Command commandBehavioral = createBehavioralCommand(subCommand);
+            break;
+         case "CREATE":
+            subCommand = createSubCommand(commandArray);
+            // this is where you would do this.parserHelper.getActionProcessor().schedule(createCreationalCommand(subCommand));
+            A_Command commandCreational = createCreationalCommand(subCommand);
+            break;
+         case "@EXIT":
+            // this is where you would do this.parserHelper.getActionProcessor().schedule(createExitCommand());
+            A_Command commandExit = createExitCommand();
+            break;
+         case "@RUN":
+            subCommand = createSubCommand(commandArray);
+            // this is where you would do this.parserHelper.getActionProcessor().schedule(createMetaRunCommand(subCommand));
+            A_Command commandMetaRun = createMetaRunCommand(subCommand);
+            break;
+         case "@SCHEDULE":
+            subCommand = createSubCommand(commandArray);
+            // this is where you would do this.parserHelper.getActionProcessor().schedule(createMetaSchedule(subCommand));
+            A_Command commandMetaSchedule = createMetaSchedule(subCommand);
+            break;
+         case "OPEN":
+            subCommand = createSubCommand(commandArray);
+            // this is where you would do this.parserHelper.getActionProcessor().schedule(createMetaView(subCommand));
+            A_Command commandMetaView = createMetaView(subCommand);
+            break;
+         case "CLOSE":
+            subCommand = createSubCommand(commandArray);
+            // this is where you would do this.parserHelper.getActionProcessor().schedule(createMetaViewDestroy(subCommand));
+            A_Command commandMetaViewDestroy = createMetaViewDestroy(subCommand);
+            break;
+         case "COMMIT":
+            // this is where you would do this.parserHelper.getActionProcessor().schedule(createStructuralCommit());
+            A_Command commandStructuralCommit = createStructuralCommit();
+            break;
+         case "COUPLE":
+            subCommand = createSubCommand(commandArray);
+            // this is where you would do this.parserHelper.getActionProcessor().schedule(createStructuralCouple(subCommand));
+            A_Command commandStructuralCouple = createStructuralCouple(subCommand);
+            break;
+         case "LOCATE":
+            subCommand = createSubCommand(commandArray);
+            // this is where you would do this.parserHelper.getActionProcessor().schedule(createStructuralLocate(subCommand));
+            A_Command commandStructuralLocate = createStructuralLocate(subCommand);
+            break;
+         case "UNCOUPLE":
+            subCommand = createSubCommand(commandArray);
+            // this is where you would do this.parserHelper.getActionProcessor().schedule(createStructuralUncouple(subCommand));
+            A_Command commandStructuralUncouple = createStructuralUncouple(subCommand);
+            break;
+         case "USE":
+            // need to have the parserHelper in order to add a reference
+            // this will be completed later
+            break;
+         default:
+            throw new IllegalArgumentException("Invalid Command");
+      }
+   }
+   
+   // helper method that strips the first element of the array and returns
+   // a string with the rest of the elements
+   private String createSubCommand(String[] commandArray)
+   {
+      StringBuilder subCommand = new StringBuilder();
+      for(int i = 1; i < commandArray.length; i++)
+      {
+         subCommand.append(commandArray[i]);
+         if(i < commandArray.length-1)
+            subCommand.append(" ");
+      }
+      return subCommand.toString();
+   }  
+   
+   /*public A_ParserHelper getParserHelper()
+   {
+      return this.parserHelper;
+   }*/
+   
+   public String getCommandText()
+   {
+      return this.commandText;
+   }
+   
+   /*
+    * @Author Marco Karier
+    * @param subCoomand with out the intial word
+    *
+    *This Method is called after the first word is DO
+    */
+   private A_Command createBehavioralCommand(String subCommand)
+   {
+      String[] commandArray = subCommand.split("\\s+");
+      subCommand = createSubCommand(commandArray);
+      A_Command command = null;
+      switch(commandArray[0])
+      {
+      	case "BRAKE":
+      		command = createBehavioralCommandBrake(subCommand);
+      		break;
+      	case "SELECT":
+      		command = createBehavioralCommandSelect(subCommand);
+      		break;
+      	case "SET"://TODO:
+      		command = createBehavioralCommandSet(subCommand);
+      		break;
+      	default:
+      		return null;
+      }//end of switch
+      return command;
+   }//end createBehavioralCommand
+   
+   /*
+    * @Author Marco Karier
+    * @Param a substring of commands
+    * 
+    * This Method is creating the command behavioral brake
+    */
+   private A_Command createBehavioralCommandBrake(String subCommand)
+   {
+	   String[] commandArray = subCommand.split("\\s+");
+	   subCommand = createSubCommand(commandArray);
+	   A_Command command = new CommandBehavioralBrake(commandArray[0]);
+	   return command;
+   }//end of createBehavioralCommandBrake
+   
+   /*
+    * @Author Marco Karier
+    * @Param a substring of commands when the second word was select
+    * 
+    * this method create a commandBehavioralSelect and checks to find which kind of select
+    */
+   private A_Command createBehavioralCommandSelect(String subCommand)
+   {
+	   String[] commandArray = subCommand.split("\\s+");
+	   subCommand = createSubCommand(commandArray);
+	   A_Command command = null;
+	   switch(commandArray[0])
+	   {
+	   	case"DRAWBRIDGE":
+		   command = createBehavioralCommandSelectDrawbridge(subCommand);
+		   break;
+	   	case"ROUNDHOUSE":
+	   		command = createBehavioralCommandSelectRoundhouse(subCommand);
+	   		break;
+	   	case"SWITCH":
+	   		command = createBehavioralCommandSelectSwitch(subCommand);
+	   }//end of switch
+	   return command;
+   }//end of createBehavioralCommandSelect()
+   
+   /*
+    * @Author Marco Karier
+    * @Param command subString
+    * 
+    * This method takes a substring and creates  drawbridge command
+    */
+   private A_Command createBehavioralCommandSelectDrawbridge(String subCommand)
+   {
+	   String[] commandArray = subCommand.split("\\s+");
+	   A_Command command = new CommandBehavioralSelectBridge(commandArray[0], commandArray[2].equals("UP"));
+	   return command;
+   }//end of createBehavioralCommandSelectDrawbridge()
+   
+   /*
+    *@Author Marco Karier
+    *@param commandSubstring
+    *This method takes a subCommandstring and outputs a CommandBehavioralSelectRoundhouse; 
+    */
+   private A_Command createBehavioralCommandSelectRoundhouse(String subCommand)
+   {
+	   String[] commandArray = subCommand.split("\\s+");
+	   double angle = Double.parseDouble(commandArray[2]);
+	   A_Command command = new CommandBehavioralSelectRoundhouse(commandArray[0], new Angle(angle), commandArray[3].equals("CLOCKWISE"));
+	   return command;
+   }//end of creawteBehavioralCommandSelectRoundhouse
+   
+   /*
+    * @Author Marco Karier
+    * @Param command Substring
+    * 
+    * This method takes a substring and outputs a CommandBehavioralSelectSwitch
+    */
+   private A_Command createBehavioralCommandSelectSwitch(String subCommand)
+   {
+	   String[] commandArray = subCommand.split("\\s+");
+	   A_Command command = new CommandBehavioralSelectSwitch(commandArray[0], commandArray[2].equals("PRIMARY"));
+	   return command;
+   }//end of createBehavioralCommandSelectSwitch
+   
+   /*
+    * @Author Marco Karier
+    * @Param command subString
+    * 
+    * this takes a string and finds what kind of set command the user wants
+    */
+   private A_Command createBehavioralCommandSet(String subString)
+   {
+	   String[] commandArray = subString.split("\\s+");
+	   subString = createSubCommand(commandArray);
+	   A_Command command = null;
+	   if(commandArray[0].equals("REFERENCE"))
+	   {
+		   command = new CommandBehavioralSetReference(commandArray[1]);
+	   }//end of if
+	   else
+	   {
+		   switch(commandArray[1])
+		   {
+		   	case"DIRECTION":
+		   		command = createBehavioralCommandSetDirection(commandArray);
+		   		break;
+		   	case"SPEED":
+		   		command = createBehavioralCommandSetSpeed(commandArray);
+		   }//end of switch
+	   }//end of else
+	   return command;
+   }//end of createBehavioralCommandSet()
+   
+   /*
+    * @Author Marco Karier
+    * @Param a string array to create the variables needed for the constructor of CommandBehavioralSetDirection()
+    * 
+    * This method is going to create a BehabioralCommandSetDirection
+    */
+   private A_Command createBehavioralCommandSetDirection(String[] commandArray)
+   {
+	   return new CommandBehavioralSetDirection(commandArray[0], commandArray[2].equals("FORWARD"));
+   }//end of createBehavioralCommandSetDirection(String[] commandArray)
+   
+   /*
+    * @Author Marco Karier
+    * @Param a string Array used to create a CommandBehavioralSetSpeed
+    * 
+    * This returns a CommandBehavioralSetSpeed object
+    */
+   private A_Command createBehavioralCommandSetSpeed(String[] commandArray)
+   {
+	   return new CommandBehavioralSetSpeed(commandArray[0], Double.parseDouble(commandArray[2]));
+   }//end of createBehavioralCommandSetSpeed
+   
+   
+   // splits the creationals commands further
+   private A_Command createCreationalCommand(String subCommand)
+   {
+      String[] commandArray = subCommand.split("\\s+");
+      A_Command command = null;
+      switch (commandArray[0].toUpperCase()){
+         case "POWER":
+            subCommand = createSubCommand(commandArray);
+            
+            command = createPowerCommand(subCommand);
+            break;
+         case "STOCK":
+            subCommand = createSubCommand(commandArray);
+            
+            command = createStockCommand(subCommand);
+            break;
+      }
+      return command;
+   }
+   
+   // creates a creational power command and returns it
+   private A_Command createPowerCommand(String subCommand)
+   {
+      String[] commandArray = subCommand.split("[()]");
+      StringBuilder strBuild = new StringBuilder();
+      for(int i = 0; i < commandArray.length; i++)
+      {
+         strBuild.append(commandArray[i]);
+         if(i < commandArray.length-1)
+             strBuild.append(" ");
+      }
+      subCommand = strBuild.toString();
+      commandArray = subCommand.split("\\s+");
+      A_Command command = null;
+      switch (commandArray[0].toUpperCase()){
+        case "CATENARY":
+           List<String> poles = new ArrayList<String>();
+           for(int i = 4; i < commandArray.length; i++)
+           {
+               poles.add(commandArray[i]);
+           }
+           command = new CommandCreatePowerCatenary(commandArray[1], poles);
+           break;
+        case "POLE":
+           boolean isFromStart = true;
+           switch (commandArray[8].toUpperCase()){
+               case "START":
+                 isFromStart = true;
+                 break;
+               case "END":
+                 isFromStart = false;
+           }
+           TrackLocator trLoc = new TrackLocator(commandArray[4], Double.parseDouble(commandArray[6]), isFromStart);
+           command = new CommandCreatePowerPole(commandArray[2], trLoc);
+           break;
+        case "STATION":
+           command = createCommandStation(subCommand);
+           // command = new CommandCreatePowerCatenary(commandArray[1], poles);
+           break;
+      }
+      return command;
+   }
+   
+   // creates a station and returns it
+   private A_Command createCommandStation(String subCommand)
+   {
+      String[] commandArray = subCommand.split("\\s+");
+      for(int i = 0; i < commandArray.length; i++)
+      {
+         System.out.println(commandArray[i]);
+      }
+      
+      return null;
+   }
+   
+   
+   // creates creational stock command and returns it
+   private A_Command createStockCommand(String subCommand)
+   {
+      String[] commandArray = subCommand.split("\\s+");
+      A_Command command = null;
+      if(commandArray[0].toUpperCase().equals("ENGINE"))
+      {
+            commandArray = subCommand.split("[()]");
+            StringBuilder strBuild = new StringBuilder();
+            for(int i = 0; i < commandArray.length; i++)
+            {
+                strBuild.append(commandArray[i]);
+                if(i < commandArray.length-1)
+                     strBuild.append(" ");
+            }
+            subCommand = strBuild.toString();
+            commandArray = subCommand.split("\\s+");
+            boolean isFromStart = true, isFacing = true;
+            switch (commandArray[10].toUpperCase()){
+                case "START":
+                  isFromStart = true;
+                  break;
+                case "END":
+                  isFromStart = false;
+            }
+            switch (commandArray[12].toUpperCase()){
+                case "START":
+                  isFacing = true;
+                  break;
+                case "END":
+                  isFacing = false;
+            }
+            TrackLocator trLoc = new TrackLocator(commandArray[6], Double.parseDouble(commandArray[8]), isFromStart);
+            command = new CommandCreateStockEngineDiesel(commandArray[1], trLoc, isFacing);
+      }
+      else
+      {
+         subCommand = createSubCommand(commandArray);
+         commandArray = subCommand.split("\\s+");
+         
+         switch (commandArray[commandArray.length-1].toUpperCase()){
+            case "BOX":
+               command = new CommandCreateStockCarBox(commandArray[0]);            
+               break;
+            case "CABOOSE":
+               command = new CommandCreateStockCarCaboose(commandArray[0]);
+               break;
+            case "FLATBED":
+               command = new CommandCreateStockCarFlatbed(commandArray[0]);
+               break;
+            case "PASSENGER":
+               command = new CommandCreateStockCarPassenger(commandArray[0]);
+               break;
+            case "TANK":
+               command = new CommandCreateStockCarTank(commandArray[0]);
+               break;
+            case "TENDER":
+               command = new CommandCreateStockCarTender(commandArray[0]);
+               break;   
+         }
+      }
+      return command;
+   }
+   
+   // creates and returns CommandMetaDoMetaExit()
+   private A_Command createExitCommand()
+   {
+      return new CommandMetaDoExit();
+   }
+   
+   // returns a CommandMetaDoMetaRun command
+   private A_Command createMetaRunCommand(String subCommand)
+   {
+      String[] commandArray = subCommand.split("\\s+");
+      return null;
+   }
+   
+   // returns a CommandMetaDoMetaSchedule command
+   private A_Command createMetaSchedule(String subCommand)
+   {
+      String[] commandArray = subCommand.split("\\s+");
+      return null;
+   }
+   
+   // returns a CommandMetaViewGenerate command
+   private A_Command createMetaView(String subCommand)
+   {
+      String[] commandArray = subCommand.split("\\s+");
+      return null;
+   }
+   
+   // returns a CommandMetaViewDestroy command
+   private A_Command createMetaViewDestroy(String subCommand)
+   {
+      String[] commandArray = subCommand.split("\\s+");
+      return null;
+   }
+   
+   // creates and returns CommandStructuralCommit
+   private A_Command createStructuralCommit()
+   {
+      return new CommandStructuralCommit();
+   }
+   
+   // returns a CommandStructuralCouple command
+   private A_Command createStructuralCouple(String subCommand)
+   {
+      String[] commandArray = subCommand.split("\\s+");
+      return null;
+   }
+   
+   // returns a CommandStructuralLocate command
+   private A_Command createStructuralLocate(String subCommand)
+   {
+      String[] commandArray = subCommand.split("\\s+");
+      return null;
+   }
+   
+   // returns a CommandStructuralUncouple command
+   private A_Command createStructuralUncouple(String subCommand)
+   {
+      String[] commandArray = subCommand.split("\\s+");
+      return null;
+   }
+}
